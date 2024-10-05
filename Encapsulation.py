9429ebcbeedefc04b1ae8f1e1f7676ab593e2055
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-04 23:27:37 -0700

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,1 +1,18 @@
+#__ for private _ for protected
+class Password:
+    def __init__(self, chars):
+        self.__chars = chars
+
+    def setter_chars(self, morechars):
+        self.__chars += morechars
+
+    def getter_chars(self):
+        return self.__chars
+
+
+pw = Password('abc')
+pw.setter_chars('123')
+print(pw.getter_chars())
+
+
 
